9d347024cae846c5523c8b15d8f00d743864ff98
Daud Olarewaju <olarewajuyahyadaud@gmail.com>
2024-05-19 12:47:45 +0100

    fix

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { PrismaService } from '../prisma.service';
 import { UserController } from './user.controller';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
-  providers: [UserService, PrismaService],
+  providers: [UserService, PrismaService, JwtService],
   exports: [UserService],
   controllers: [UserController],
 })
